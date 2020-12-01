b1e44ce61df1a58f403ae94f6717afab044168df
Adib Hurtado Olson <adibhurtadoolson@gmail.com>
2020-12-01 13:14:31 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '04f09ebcd96b1376ba57d092277d8120bd6a8fe2a72850606cd096018bc36be9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'success'
+
+from app import routes
